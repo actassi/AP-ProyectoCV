42cdc1583199640b0237eeac6e32e6edf636e941
actassi <actassi@gmail.com>
2022-08-17 05:21:30 -0300

    fix header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
     LogoAPComponent,
     RedesComponent,
     BannerComponent,
-    PerfilComponent
+    PerfilComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
